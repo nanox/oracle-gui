a749f0097bf6c65d97861ed4c3cc7e6c0a8463f5
sabuj.das <sabuj.das@bb137bbe-781c-11de-b8b8-6d0436ffb0fe>
2010-02-19 21:21:11 +0000

    jdbc driver loader

---
+++ trunk/database-explorer/dbex/dbex-application/src/main/java/com/gs/dbex/application/constants/ApplicationConstants.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package com.gs.dbex.application.constants;
+
+/**
+ * @author sabuj
+ *
+ */
+public interface ApplicationConstants {
+
+	String IMAGE_LOCATION = "/images/";
+	
+	String DEFAULT_APP_DATA_DIR = "./application-data/";
+	
+	
+}
