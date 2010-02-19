366655ec2a41d240584a642d0050696f9c56128e
sabuj.das <sabuj.das@bb137bbe-781c-11de-b8b8-6d0436ffb0fe>
2010-02-19 21:21:11 +0000

    jdbc driver loader

---
+++ database-explorer/dbex/dbex-application/src/main/java/com/gs/dbex/application/constants/ImageConstants.java
@@ -0,0 +1,33 @@
+/**
+ * 
+ */
+package com.gs.dbex.application.constants;
+
+/**
+ * @author sabuj
+ * 
+ */
+public interface ImageConstants extends ApplicationConstants {
+
+	/**
+	 * Image location for Common Application
+	 */
+	String APPLICATION_IMG_LOC = IMAGE_LOCATION + "com/gs/dbex/application/";
+	/**
+	 * Image location for Connection Manager
+	 */
+	String CONNECTION_MGR_IMG_LOC = IMAGE_LOCATION
+			+ "com/gs/dbex/application/connection/";
+	/**
+	 * Image Location for Driver Manager
+	 */
+	String DRIVER_MGR_IMG_LOC = IMAGE_LOCATION
+			+ "com/gs/dbex/application/connection/driver/";
+
+	String DRIVER_IMG_NAME = "driver.png";
+	String JAR_IMG_NAME = "jar_file.png";
+
+	String CONNECTION_MGR_IMG_NAME = "Connection_Manager.png";
+
+	String HOUR_GLASS_IMG_NAME = "Connection_Manager.png";
+}
---
+++ database-explorer/dbex/dbex-application/src/main/java/com/gs/dbex/application/context/ApplicationCommonContext.java
@@ -0,0 +1,78 @@
+/**
+ * 
+ */
+package com.gs.dbex.application.context;
+
+import com.gs.dbex.application.constants.ApplicationConstants;
+
+/**
+ * A singleton class to hold the runtime context
+ * 
+ * @author sabuj
+ *
+ */
+public class ApplicationCommonContext{
+
+	private static final String DRIVER_MGR_DIR = "driver-manager/";
+	private static final String JDBC_DRIVER_DIR = DRIVER_MGR_DIR + "jdbc-driver/";
+	private static final String DRIVER_MGR_DATA_FILE = "jdbc-driver-manager.xml";
+
+	private static ApplicationCommonContext instance;
+	
+	private ApplicationCommonContext(){}
+
+	public static ApplicationCommonContext getInstance() {
+		if(instance == null)
+			instance = new ApplicationCommonContext();
+		return instance;
+	}
+	
+	protected void finalize() throws Throwable {
+		super.finalize();
+	}
+	
+	public static void resetContext(){
+		instance = new ApplicationCommonContext();
+	}
+	
+	/* -----  Context Variables   ------------------------------------------ */
+	
+	private String applicationDataDirName = ApplicationConstants.DEFAULT_APP_DATA_DIR;
+	private String driverLoaderLastAccessedDirName = "./";
+
+	private String driverManagerDir = getApplicationDataDirName() + DRIVER_MGR_DIR;
+	private String jdbcDriverDir = getApplicationDataDirName() + JDBC_DRIVER_DIR;
+	private String driverMgrDataFileName = getDriverManagerDir() + DRIVER_MGR_DATA_FILE;
+		
+	/* -----  get/set methods for Context Variables  ----------------------- */
+	
+	public String getApplicationDataDirName() {
+		return applicationDataDirName;
+	}
+
+	public void setApplicationDataDirName(String applicationDataDirName) {
+		this.applicationDataDirName = applicationDataDirName;
+	}
+
+	public String getDriverManagerDir() {
+		return driverManagerDir;
+	}
+
+	public String getJdbcDriverDir() {
+		return jdbcDriverDir;
+	}
+
+	public String getDriverMgrDataFileName() {
+		return driverMgrDataFileName;
+	}
+
+	public String getDriverLoaderLastAccessedDirName() {
+		return driverLoaderLastAccessedDirName;
+	}
+
+	public void setDriverLoaderLastAccessedDirName(
+			String driverLoaderLastAccessedDirName) {
+		this.driverLoaderLastAccessedDirName = driverLoaderLastAccessedDirName;
+	} 
+
+}
