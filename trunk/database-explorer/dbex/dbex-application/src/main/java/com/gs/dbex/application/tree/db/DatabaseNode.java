6541ddaffc6839b9cc2791cf3998ddc26e8cbe80
sabuj.das <sabuj.das@bb137bbe-781c-11de-b8b8-6d0436ffb0fe>
2010-08-19 16:17:11 +0000

    Created a new project in google code.

--- trunk/database-explorer/dbex/dbex-application/src/main/java/com/gs/dbex/application/tree/db/DatabaseNode.java
+++
@@ -1,7 +0,0 @@
-package com.gs.dbex.application.tree.db;
-
-import javax.swing.tree.DefaultMutableTreeNode;
-
-public interface DatabaseNode<T>{
-	public boolean expand(DefaultMutableTreeNode parent);
-}
